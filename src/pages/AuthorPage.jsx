73f749dadb75f45a95309d35320febecdbefbed4
Shahab Cypher <shahabcypher@gmail.com>
2025-08-05 03:26:45 +0330

    added get an author info query

--- src/pages/AuthorPage.jsx
+++ src/pages/AuthorPage.jsx
@@ -1,4 +1,15 @@
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+import { GET_AUTHOR_INFO } from "../graphql/queries";
+
 const AuthorPage = () => {
+  const { slug } = useParams();
+
+  const { loading, error, data } = useQuery(GET_AUTHOR_INFO, {
+    variables: { slug },
+  });
+
   return <div>AuthorPage</div>;
 };
 
