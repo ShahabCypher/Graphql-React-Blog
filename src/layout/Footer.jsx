612a40802b8afa890f1e21e66190951b589f90b5
Shahab Cypher <shahabcypher@gmail.com>
2025-08-04 22:11:27 +0330

    added footer to layout

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,21 @@
+import { Typography } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Typography
+        component="p"
+        variant="p"
+        bgcolor="#f7f7f7"
+        color="primary"
+        padding="10px"
+        textAlign="center"
+        mt={10}
+      >
+        Sample Blog Project with GraphQL
+      </Typography>
+    </footer>
+  );
+};
+
+export default Footer;
