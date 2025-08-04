681a4b1c247eb9cbd1639f473d1ce423efd69ce2
Shahab Cypher <shahabcypher@gmail.com>
2025-08-05 02:55:13 +0330

    added get authors data query

--- src/components/authors/Authors.jsx
+++ src/components/authors/Authors.jsx
@@ -1,4 +1,14 @@
+import { useQuery } from "@apollo/client";
+
+import { GET_AUTHORS_INFO } from "../../graphql/queries";
+
 const Authors = () => {
+  const { loading, error, data } = useQuery(GET_AUTHORS_INFO);
+
+  if (loading) return <h4>Loading...</h4>;
+
+  if (error) return <h4>Something went wrong</h4>;
+
   return <div>Authors</div>;
 };
 
