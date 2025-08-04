2a9bb8f413e4f47bda527cf67ae128c458f499b3
Shahab Cypher <shahabcypher@gmail.com>
2025-08-04 05:15:42 +0330

    added apollo client to use hygraph

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,17 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { createRoot } from "react-dom/client";
+
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+const HYGRAPH_URL = import.meta.env.VITE_HYGRAPH_URL;
+
+const client = new ApolloClient({
+  uri: HYGRAPH_URL,
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
