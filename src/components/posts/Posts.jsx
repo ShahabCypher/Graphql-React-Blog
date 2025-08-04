e602ad137c50efa2955ae2a3e66ab4c108456e02
Shahab Cypher <shahabcypher@gmail.com>
2025-08-04 22:48:37 +0330

    added get posts query

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,5 +1,15 @@
+import { useQuery } from "@apollo/client";
+
+import { GET_BLOGS_INFO } from "../../graphql/queries";
+
 const Posts = () => {
-  return <div>Posts</div>;
+  const { loading, error, data } = useQuery(GET_BLOGS_INFO);
+
+  if (loading) return <h4>Loading...</h4>;
+
+  if (error) return <h4>Something went wrong</h4>;
+
+  return <div></div>;
 };
 
 export default Posts;
