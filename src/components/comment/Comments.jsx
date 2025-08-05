70c63126104aedd2032ad4bdaaec5de7412bdfbb
Shahab Cypher <shahabcypher@gmail.com>
2025-08-05 05:45:10 +0330

    created show comments component

--- src/components/comment/Comments.jsx
+++ src/components/comment/Comments.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
+import { Avatar, Box, Grid, Typography } from "@mui/material";
 
-import Loader from "../Loader";
 import { GET_POST_COMMENTS } from "../../graphql/queries";
 
 const Comments = ({ slug }) => {
@@ -8,7 +8,46 @@ const Comments = ({ slug }) => {
     variables: { slug },
   });
 
-  return <div>Comments</div>;
+  if (loading) return;
+
+  const { comments } = data;
+
+  return (
+    <Grid
+      container
+      sx={{
+        boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px",
+        borderRadius: 4,
+        py: 1,
+        mt: 8,
+      }}
+    >
+      <Grid size={{ xs: 12 }} m={2}>
+        <Typography component="p" variant="h6" fontWeight={700} color="primary">
+          Comments
+        </Typography>
+        {comments.map((comment) => (
+          <Grid
+            key={comment.id}
+            m={2}
+            p={2}
+            border="1px silver solid"
+            borderRadius={1}
+          >
+            <Box component="div" display="flex" alignItems="center" mb={3}>
+              <Avatar>{comment.name[0]}</Avatar>
+              <Typography component="span" variant="p" fontWeight={700} ml={1}>
+                {comment.name}
+              </Typography>
+            </Box>
+            <Typography component="p" variant="p">
+              {comment.text}
+            </Typography>
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Comments;
