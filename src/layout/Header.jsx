71eeebba3adabac71404f101725e40825d3de33f
Shahab Cypher <shahabcypher@gmail.com>
2025-08-05 06:06:00 +0330

    added header links

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,15 +1,26 @@
 import { AppBar, Toolbar, Typography, Container } from "@mui/material";
 import BookIcon from "@mui/icons-material/Book";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <AppBar position="sticky">
       <Container maxWidth="lg">
         <Toolbar>
-          <Typography component="h1" variant="h5" fontWeight="bold" flex={1}>
-            React Graphql Blog
+          <Typography
+            component="h1"
+            variant="h5"
+            fontWeight="bold"
+            flex={1}
+            sx={{ userSelect: "none" }}
+          >
+            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+              React Graphql Blog
+            </Link>
           </Typography>
-          <BookIcon />
+          <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+            <BookIcon />
+          </Link>
         </Toolbar>
       </Container>
     </AppBar>
